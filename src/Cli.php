373b5ffa7a704ba813bdacc820f05a526419a022
Anna Yashina <sowa84@bk.ru>
2018-10-16 19:43:53 +0400

    update 2 step

--- src/Cli.php
+++ src/Cli.php
@@ -3,10 +3,11 @@
 namespace BrainGames\Cli;
 
 use function \cli\line;
+use function \cli\prompt;
 
 function run()
 {
-      line('Welcome to the Brain Game!\n');
-      $name = \cli\prompt('May I have your name?');
+      line('Welcome to the Brain Game!');
+      $name = prompt('May I have your name?');
       line("Hello, %s!", $name);
 }
