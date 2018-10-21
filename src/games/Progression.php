a68df67950174a619152343c54288d122975dd4c
Anna Yashina <sowa84@bk.ru>
2018-10-21 10:35:11 +0400

    update 8 step

--- src/games/Progression.php
+++ src/games/Progression.php
@@ -5,29 +5,28 @@ namespace BrainGames\GameProgression;
 use function BrainGames\Process\run as run_progression;
 
 const DESCRIPTION = "What number is missing in this progression?";
-const LENGTH_PROGRESSION = 10;
+const PROGRESSION_LENGTH = 10;
 const RAND_MAX = 100;
 
 function run()
 {
     $generate = function () {
-        $needleNumber = rand(1, LENGTH_PROGRESSION);
-        $initialState = rand(1, RAND_MAX);
-        $difference = rand(1, LENGTH_PROGRESSION);
-        $question = '';
+        $needleNumber = rand(1, PROGRESSION_LENGTH);
+        $first = rand(1, RAND_MAX);
+        $difference = rand(1, PROGRESSION_LENGTH);
         $answer = '';
 
-        $sequence = [];
-        for ($i = 1; $i < LENGTH_PROGRESSION; $i++) {
-            $item = (string) ($initialState + $difference * ($i - 1));
+        $question = [];
+        for ($i = 0; $i < PROGRESSION_LENGTH; $i++) {
+            $item = (string) ($first + $difference * ($i - 1));
             if ($i === $needleNumber) {
                 $answer = $item;
                 $item = '..';
             }
-            $question = $question . $item . ' ';
+            $question[] = $item;
         }
         return [
-          substr($question, 0, -1),
+          implode($question, ' '),
           $answer
         ];
     };
